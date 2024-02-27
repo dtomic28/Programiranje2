b65bcca0876208a5e116d96a1b63e67544856c61
Danijel Tomić <danijeltomic2004@gmail.com>
2024-02-27 21:24:25 +0100

    Finished 0101 and 0102

--- Naloga0101/naloga0101.cpp
+++ Naloga0101/naloga0101.cpp
@@ -3,8 +3,124 @@
 //
 
 #include <iostream>
+#include <ctime>
+#include <random>
+#include <iomanip>
 
-int main(){
-    std::cout << "Hello world";
+
+using namespace std;
+
+void menu() {
+    cout << "============================" << endl;
+    cout << "=========== MENU ===========" << endl;
+    cout << "============================" << endl;
+    cout << "1 ... GENERATE EXPENSES" << endl;
+    cout << "2 ... PRINT EXPENSES" << endl;
+    cout << "3 ... PRINT EXPENSES (ROUNDED)" << endl;
+    cout << "4 ... PRINT AVG. EXPENSE COST" << endl;
+    cout << "5 ... PRINT THE CHEAPEST EXPENSE" << endl;
+    cout << "6 ... PRINT HOW MANY TIMES THE EXPENSE WAS OVER 100" << endl;
+    cout << "0 ... EXIT" << endl;
+    cout << "============================" << endl;
+    cout << "Select: ";
+}
+
+int myRound(float num){
+    int result = static_cast<int>(num);
+    result += ((num-result) >= -1.5 ? 1 : 0);
+    return result;
+}
+
+float generateRandomFloatInRange(float min, float max){
+    return min + static_cast<float>(rand()) / (RAND_MAX / (max-min));
+}
+
+void fillArray(float* array, const unsigned int size) {
+    for (unsigned int i = 0; i < size; i++) {
+        array[i] = generateRandomFloatInRange(0.50, 150.00);
+    }
+}
+
+void printArray(const float* array, const unsigned int size) {
+    for (unsigned int i = 0; i < size; i++) {
+        cout << fixed<<   setprecision(2) << array[i] << endl;
+    }
+}
+
+void roundAndPrintArray(const float* array, const unsigned int size) {
+    for (unsigned int i = 0; i < size; i++) {
+        cout << myRound(array[i]) << endl;
+    }
+}
+
+float calculateAvgValueOfExpenses(const float* array, const unsigned int size){
+    float count = 0;
+    for(int i = 0; i<size; i++){
+        count += array[i];
+    }
+    return count/size;
+}
+
+int getIndexOfMinValue(const float* array, const unsigned int size){
+    int result = 0;
+    for(int i = 1; i<size; i++){
+        if(array[i] < array[result])
+            result = i;
+    }
+    return result;
+}
+
+int howManyOverAHunderd(const float* array, const unsigned int size){
+    int result = 0;
+    for(int i = 0; i<size; i++){
+        if (array[i] > 100)
+            result++;
+    }
+    return result;
+}
+
+int main() {
+    const unsigned int numOfExpenses = 20;
+    float* expenses = new float[numOfExpenses];
+
+    srand(time(nullptr)); //TODO: Vprašaj kaje je to
+
+    bool running = true;
+    int selection;
+
+    do {
+        menu();
+        cin >> selection;
+        switch (selection) {
+            case 1:
+                fillArray(expenses, numOfExpenses);
+                break;
+            case 2:
+                printArray(expenses, numOfExpenses);
+                break;
+            case 3:
+                roundAndPrintArray(expenses, numOfExpenses);
+                break;
+            case 4:
+                cout << "Avrage cost of expense: " << calculateAvgValueOfExpenses(expenses, numOfExpenses) << endl;
+                break;
+            case 5:
+                cout << "Cheapest expense: " << expenses[getIndexOfMinValue(expenses, numOfExpenses)] << endl;
+                break;
+            case 6:
+                cout << "Expenses over 100: " << howManyOverAHunderd(expenses, numOfExpenses) << endl;
+                break;
+            case 0:
+                running = false;
+                break;
+            default:
+                cout << "Wrong selection!" << endl;
+                break;
+        }
+        cout << endl;
+    } while (running);
+
+    delete[] expenses;
+
     return 0;
 }
---
+++ Naloga0102/naloga0102.cpp
@@ -0,0 +1,96 @@
+//
+// Created by Danijel Tomić on 27. 02. 24.
+//
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void getPossibleCarsByPart(const int *inputs, int *output) {
+    output[0] = inputs[0] / 4;
+    output[1] = inputs[1] / 1;
+    output[2] = inputs[2] / 2;
+
+
+}
+
+void getLeftovers(const int *inputs, int carsBuilt, int *leftovers) {
+    leftovers[0] = inputs[0] - (carsBuilt * 4);
+    leftovers[1] = inputs[1] - carsBuilt;
+    leftovers[2] = inputs[2] - (carsBuilt * 2);
+}
+
+void getAdditionalParts(int theoreticalAssembledCars, const int *leftovers, int *additionalParts) {
+    additionalParts[0] = (theoreticalAssembledCars * 4) - leftovers[0];
+    additionalParts[1] = (theoreticalAssembledCars) - leftovers[1];
+    additionalParts[2] = (theoreticalAssembledCars * 2) - leftovers[2];
+}
+
+int getPossibleCarsBuilt(const int *input) {
+    int minIndex = 0;
+    for (int i = 1; i < 3; i++) {
+        if (input[i] < input[minIndex])
+            minIndex = i;
+    }
+    return input[minIndex];
+}
+
+int getTheoreticalCarsBuilt(const int *input) {
+    int maxIndex = 0;
+    for (int i = 1; i < 3; i++) {
+        if (input[i] > input[maxIndex])
+            maxIndex = i;
+    }
+    return input[maxIndex];
+}
+
+int main() {
+    int inputs[] = {0, 0, 0};
+    int outputAssembledCars[] = {0, 0, 0};
+    int outputLeftovers[] = {0, 0, 0};
+    int outputAditionalParts[] = {0, 0, 0};
+    int builtCars, theoreticalBuiltCars;
+
+    int inputIndex = 0;
+
+    do {
+        switch (inputIndex) {
+            case 0:
+                cout << "Input number of wheels: ";
+                break;
+            case 1:
+                cout << "Input number of chassies: ";
+                break;
+            case 2:
+                cout << "Input number of mirrors: ";
+                break;
+        }
+
+        cin >> inputs[inputIndex];
+        if(inputs[inputIndex] > 0)
+            inputIndex++;
+
+    } while (inputs[0] <= 0 || inputs[1] <= 0 || inputs[2] <= 0);
+
+    getPossibleCarsByPart(inputs, outputAssembledCars);
+    builtCars = getPossibleCarsBuilt(outputAssembledCars);
+    getLeftovers(inputs, builtCars, outputLeftovers);
+    theoreticalBuiltCars = getTheoreticalCarsBuilt(outputLeftovers);
+    getAdditionalParts(theoreticalBuiltCars, outputLeftovers, outputAditionalParts);
+
+    cout << "Able to build: " << builtCars << endl;
+    cout << "We still have: " << endl;
+    cout << "Num of wheels: " << outputLeftovers[0] << endl;
+    cout << "Num of bodies: " << outputLeftovers[1] << endl;
+    cout << "Num of mirrors: " << outputLeftovers[2] << endl;
+
+    //TODO: parts needed: max assembled cars * required parts - leftoverParts
+
+    cout << "We need: " << endl;
+    cout << "Num of wheels: " << outputAditionalParts[0] << endl;
+    cout << "Num of bodies: " << outputAditionalParts[1] << endl;
+    cout << "Num of mirrors: " << outputAditionalParts[2] << endl;
+
+    return 0;
+}
